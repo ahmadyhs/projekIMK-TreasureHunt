b7fae9adc71c15014c9eab6803da1c66e68e0915
angls4 <57803800+angls4@users.noreply.github.com>
2023-05-03 09:59:02 +0700

    a

--- Assets/cameraMovement.cs
+++ Assets/cameraMovement.cs
@@ -6,19 +6,20 @@ public class cameraMovement : MonoBehaviour
 {
     public Vector3 offset;
     public Vector3 lookOffset;
+    public Vector3 lookConstraint;
     public GameObject target;
     public Vector3 targetPoint;
     public float moveSpeed = 50f;
     // Start is called before the first frame update
     void Start()
     {
-        offset = transform.position - target.transform.position;
+        offset = offset + transform.position - target.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        targetPoint = target.transform.position + lookOffset;
+        targetPoint = lookOffset + new Vector3(target.transform.position.x * lookConstraint.x, target.transform.position.y * lookConstraint.y, target.transform.position.z * lookConstraint.z);
         transform.position = Vector3.Lerp(transform.position, target.transform.position + offset,Time.deltaTime * moveSpeed);
         //transform.LookAt(targetPoint);
         transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(targetPoint - this.transform.position,Vector3.up),Time.deltaTime * moveSpeed*0.5f);
