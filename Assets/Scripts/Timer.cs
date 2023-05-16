b02808a5a3340684652c5cf843cc373a81193601
bagus2705 <bagusprasetyo2705@gmail.com>
2023-05-16 10:58:15 +0700

    Timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public TextMeshProUGUI timer;
+    private float startTime;
+   
+
+    void Start()
+    {
+        startTime = Time.time;
+  
+    }
+
+    void Update()
+    {
+       
+            float t = Time.time - startTime;
+
+            string minutes = ((int)t / 60).ToString();
+            string seconds = (t % 60).ToString("f2");
+
+            timer.text = minutes + ":" + seconds;
+    
+    }
+}
