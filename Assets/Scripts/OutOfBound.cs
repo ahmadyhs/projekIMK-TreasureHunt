e4e2167860f102ede67cf53912a93fad07b219d1
bagus2705 <bagusprasetyo2705@gmail.com>
2023-05-18 19:41:05 +0700

    Respawn udh bener

--- Assets/Scripts/OutOfBound.cs
+++ Assets/Scripts/OutOfBound.cs
@@ -2,19 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 
 public class OutOfBound : MonoBehaviour
 {
         public int maxLives = 3;
         private int remainingLives;
-
+        private Vector3 startingPosition;
+        public TextMeshProUGUI counter;
+        public TextMeshProUGUI lives;
         public Transform respawnPosition;
         public SceneReference targetScene;
         private void Start()
         {
             remainingLives = maxLives;
-        }
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            startingPosition = player.transform.position;
+    }
 
         private void OnTriggerEnter(Collider other)
         {
@@ -23,7 +28,12 @@ public class OutOfBound : MonoBehaviour
             Debug.Log("kena");
                 ReduceLife();
             }
+        if (other.CompareTag("Trap"))
+        {
+            Debug.Log("trap");
+            ReduceLife();
         }
+    }
 
         private void ReduceLife()
         {
@@ -31,23 +41,28 @@ public class OutOfBound : MonoBehaviour
 
             if (remainingLives <= 0)
             {
+            PlayerPrefs.SetString("CoinCollected", counter.text);
+            GameObject.Find("character").SendMessage("StopTimerAndStoreTime");
             SceneManager.LoadScene(targetScene);
             }
             else
             {
+                lives.text = remainingLives.ToString();
                 RespawnPlayer();
             }
         }
 
         private void RespawnPlayer()
         {
+        
+        Debug.Log("detect");
         GameObject player = GameObject.FindGameObjectWithTag("Player");
         Rigidbody playerRigidbody = player.GetComponent<Rigidbody>();
 
-        playerRigidbody.velocity = Vector3.zero; // Reset velocity
-        playerRigidbody.angularVelocity = Vector3.zero; // Reset angular velocity
+        playerRigidbody.velocity = Vector3.zero; 
+        playerRigidbody.angularVelocity = Vector3.zero; 
 
-        player.transform.position = respawnPosition.position;
+        player.transform.position = startingPosition;
     }
     }
 
